e52a51dbf8d9dc4ffcfec954314013785a1661c6
moonfr1end <aidostinimbaev2@gmail.com>
2022-06-27 03:00:48 +0600

    rl v1

---
+++ js/common.js
@@ -0,0 +1,59 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+
+    // type: 'fraction',
+    // renderFraction: function(currentClass, totalClass) {
+    // 	return '<span>' + currentClass + '</span'+
+    // 	 			 '<span>' + totalClass + '</span';
+    // },
+
+    type: 'progressbar',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    enabled: 'true'
+  },
+});
